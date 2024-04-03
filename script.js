5cbb320b26a5d9ea22b813fe2176e2c4767952fe
Anjalivlog <139326744+Anjalivlog@users.noreply.github.com>
2024-04-03 15:53:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const timerElement = document.getElementById('timer');
+function updateTimer() {
+	const now = new Date();
+	const dateTimeString = now.toLocaleString();
+	timerElement.textContent = dateTimeString;
+}
+setInterval(updateTimer,1000);
+updateTimer();
